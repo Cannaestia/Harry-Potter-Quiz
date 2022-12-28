f8bb06dd56f1a61d8833875d98c013a8ed2f80bf
Jennifer Arnold <cannaestia@gmail.com>
2022-12-27 21:27:57 -0500

    updated music

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,87 @@ var cursor = 0;
 var score = 0;
 var timeLeft = 100;
 
+// Get a reference to the button and audio element
+var audioEl = document.getElementById("myAudio")
+var playButton = document.getElementById("magic");
+var audio = document.getElementById("myAudio");
+
+// Add an event listener to the button that will play the audio when clicked
+playButton.addEventListener("click", function() {
+  audio.play();
+});
+audioEl.addEventListener("load", function(e) {
+  audio.play();
+});
+
+if (document.body.animate) {
+  document.querySelector('#magic').addEventListener('click', pop);
+}
+
+function pop (e) {
+  // Quick check if user clicked the button using a keyboard
+  if (e.clientX === 0 && e.clientY === 0) {
+    const bbox = document.querySelector('#magic').getBoundingClientRect();
+    const x = bbox.left + bbox.width / 2;
+    const y = bbox.top + bbox.height / 2;
+    for (let i = 0; i < 30; i++) {
+      // We call the function createParticle 30 times
+      // We pass the coordinates of the button for x & y values
+      createParticle(x, y);
+    }
+  } else {
+    for (let i = 0; i < 30; i++) {
+      // We call the function createParticle 30 times
+      // As we need the coordinates of the mouse, we pass them as arguments
+      createParticle(e.clientX, e.clientY);
+    }
+  }
+}
+
+function createParticle (x, y) {
+  const particle = document.createElement('particle');
+  document.body.appendChild(particle);
+  
+  // Calculate a random size from 5px to 25px
+  const size = Math.floor(Math.random() * 20 + 5);
+  particle.style.width = `${size}px`;
+  particle.style.height = `${size}px`;
+  // Generate a random color in a blue/purple palette
+  particle.style.background = `hsl(${Math.random() * 90 + 180}, 70%, 60%)`;
+  
+  // Generate a random x & y destination within a distance of 75px from the mouse
+  const destinationX = x + (Math.random() - 0.5) * 2 * 75;
+  const destinationY = y + (Math.random() - 0.5) * 2 * 75;
+
+  // Store the animation in a variable as we will need it later
+  const animation = particle.animate([
+    {
+      // Set the origin position of the particle
+      // We offset the particle with half its size to center it around the mouse
+      transform: `translate(-50%, -50%) translate(${x}px, ${y}px)`,
+      opacity: 1
+    },
+    {
+      // We define the final coordinates as the second keyframe
+      transform: `translate(${destinationX}px, ${destinationY}px)`,
+      opacity: 0
+    }
+  ], {
+    // Set a random duration from 500 to 1500ms
+    duration: Math.random() * 1000 + 500,
+    easing: 'cubic-bezier(0, .9, .57, 1)',
+    // Delay every particle with a random value of 200ms
+    delay: Math.random() * 200
+  });
+  
+  // When the animation is complete, remove the element from the DOM
+  animation.onfinish = () => {
+    particle.remove();
+  };
+}
+
+
+
 // Questions for the quiz
 
 var questions = [
@@ -143,6 +224,32 @@ function nextQuestion(event) {
   }
 }
 
+// function fadeInOut(pageIn, pageOut) {
+//   // Set the opacity of the incoming page to 0
+//   pageIn.style.opacity = 0;
+//   // Show the incoming page
+//   pageIn.style.display = "block";
+//   // Fade in the incoming page
+//   var fadeInInterval = setInterval(function() {
+//     if (pageIn.style.opacity < 1) {
+//       pageIn.style.opacity = parseFloat(pageIn.style.opacity) + 0.1;
+//     } else {
+//       clearInterval(fadeInInterval);
+//     }
+//   }, 100);
+//   // Fade out the outgoing page
+//   var fadeOutInterval = setInterval(function() {
+//     if (pageOut.style.opacity > 0) {
+//       pageOut.style.opacity = parseFloat(pageOut.style.opacity) - 0.1;
+//     } else {
+//       clearInterval(fadeOutInterval);
+//       pageOut.style.display = "none";
+//     }
+//   }, 100);
+// }
+
+
+
 // These determin what part of the quiz is showing pased on what part of the quiz you are on ie Start, Quiz, or End 
 
 function showQuestions() {
@@ -172,8 +279,13 @@ function startTimer() {
       clearInterval(timeInterval);
       endQuiz()
     }
-  }, 1000);
+  },1000);
 }
+
+// function playAudio (audio) {
+//   var audio = document.getElementById("audio");
+//   audio.play();
+// }
 
 startButton.addEventListener("click", function () {
   showQuestions();
@@ -224,8 +336,6 @@ submitEl.addEventListener("click", function(event) {
   }
   resetEl.addEventListener("click",reset)
 
-  window.onload = function () {
-    document.getElementById("audio").play();
-}
-  
-  
+
+// var audioEl = document.querySelector('#audio');
+
